b1ae40c4a1c3a85204287cc6124f75508c8e7449
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-06-22 17:10:45 +0300

    Finished #166858038 Rectified the uris

--- js/post_property.js
+++ js/post_property.js
@@ -1,23 +1,3 @@
-// var imgPreview = document.getElementById('img-preview');
-// var fileUpload = document.getElementById('file-upload');
-//
-// fileUpload.addEventListener('change', function(event){
-//   url = 'https://api.cloudinary.com/v1_1/<USER_ID>/image/upload';
-//   uploadPreset = '<UPLOAD_PRESET>';
-//   file = event.target.files[0];
-//   fd = new FormData();
-//   fd.append("upload_preset", uploadPreset);
-//   fd.append("file", file);
-//   config = {headers: { "X-Requested-With": "XMLHttpRequest" }};
-//
-//   axios.post(url, fd, config)
-//        .then(function(res){
-//          console.log(res);
-//        })
-//        .catch(function(err){
-//          console.error(err);
-//        });
-// });
 async function Submit(){
   let p = document.getElementById("propImg");
   let propImg = p.files[0];
@@ -47,7 +27,7 @@ let property =JSON.stringify({
 });
  console.log(property);
 let options = {method: "POST", body: property, headers:{"Content-Type": "application/json"}}
-fetch('http://127.0.0.1:3000/api/v2/properties/post-property', options)
+fetch('https://propertyproliteapi.herokuapp.com/api/v2/properties/post-property', options)
 .then(function(response) {
 return response.json();
 })
